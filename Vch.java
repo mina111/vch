b89bd17e6b235edd20bc6e558742308e1b74e05d
ben.j42 <ben.j42@a8ac134c-b99d-11de-b431-8336cfaced44>
2009-10-28 14:13:16 +0000

    And so it begins..

---
+++ Vch.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////
+/////
+/////  FullName:		Ben, Chao, Tom, Alexander, Lao
+/////  CreationDate:	2009-10-28
+/////  Module:			G52GRP
+/////
+///////////////////////////////////////////////////
+
+
+
+// IMPORT Java Libraries
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+
+public class Vch {
+
+	// And so it begins..	
+	
+}
