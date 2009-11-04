d6871e9e0f00e8b20fd9345cc36c9bc641b9d8aa
Fettor <Fettor@a8ac134c-b99d-11de-b431-8336cfaced44>
2009-11-04 01:58:54 +0000

    Very spaghetti coded graph, certainly not understandable for anyone else, however its just an example to be used tomorrow, I shall comment and rewrite a lot of it, just wanted something we can refer to tomorrow.
    Also to be tested on Lab computers as it is _VERY_ unoptimised.

---
+++ trunk/VCHUI/VCHUIFrame.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+import java.util.*;
+
+public class VCHUIFrame extends JFrame {
+	
+	public VCHUIFrame(){
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setSize(1100,900);
+		setVisible(true);
+		setTitle("VHC");
+	}
+	
+	public static void main(String args[]) {
+		VCHUIPanel vhcP = new VCHUIPanel();
+		VCHUIFrame vhcFrame = new VCHUIFrame();
+		vhcFrame.add(vhcP);	
+	}
+}
