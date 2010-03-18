dd32f026d745c8b06ed4949dd2b6eeed66e20ed8
laojingqi <laojingqi@a8ac134c-b99d-11de-b431-8336cfaced44>
2010-03-18 22:10:58 +0000

    I made the interface but there is no comments on all files and some files may be not readable. I will add comments in few days and make them readable.

---
+++ trunk/CustomizeExhibitionPanel.java
@@ -0,0 +1,99 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+
+
+public class CustomizeExhibitionPanel extends JPanel{
+
+	
+	int width,height;
+	CustomizePanel p_left;
+	JPanel  p_bar , p_right;
+
+	Config con ;
+
+	ImageIcon btn_split_img ;
+	JButton btn_split ;
+
+	VSHMainFrame frame;
+	
+	
+
+	InitalMenuPanel initalMenuPanel;
+	JPanel animationPanel;
+	MyLabel flowchart;
+
+	CustomizeExhibitionPanel(VSHMainFrame frame,int width,int height)
+	{
+		
+
+		this.width = width;
+		this.height = height;
+		this.frame = frame;
+		initPanel();
+	}
+
+	private void initPanel()
+	{
+
+		con = new Config();
+		btn_split_img = con.getImgUrl("btn_split_l.png");
+		int splitbarwidth = con.getSplitBarWidth() ; 
+		int leftwidth = con.getLeftMenuWidth() ;
+		int rightwidth = width - splitbarwidth - leftwidth ; 
+	
+		this.setPreferredSize(new Dimension(width,height));
+		this.setOpaque(false);
+		this.setLayout(con.getFlowLayout(0,0,0));
+	
+		p_left = new CustomizePanel(frame, leftwidth, height);
+	
+		p_left.setLayout(con.getFlowLayout(1,0,0)); 
+		this.add(p_left);
+	
+		p_bar = new JPanel();
+		p_bar.setPreferredSize(new Dimension(splitbarwidth,height));
+		p_bar.setBackground(con.getSplitBgColor());
+		p_bar.setLayout(con.getFlowLayout(1, 0, (height - btn_split_img.getIconHeight())/2-30));
+		this.add(p_bar);
+
+		btn_split = new JButton(btn_split_img);
+		btn_split.setPreferredSize(new Dimension(splitbarwidth,btn_split_img.getIconHeight()));
+		btn_split.setToolTipText("¹Ø±Õ");
+		btn_split.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		btn_split.setBorder(null);
+		btn_split.setFocusCycleRoot(false);
+		btn_split.setActionCommand("lmenu_close");
+		btn_split.addActionListener(new Action(frame,btn_split));
+		btn_split.setOpaque(true);
+		p_bar.add(btn_split);
+
+		p_right = new JPanel();
+		p_right.setPreferredSize(new Dimension(rightwidth,height));
+
+		p_right.setLayout(new BorderLayout());
+		initalMenuPanel = new InitalMenuPanel(rightwidth,con,frame);
+		p_right.add(initalMenuPanel,BorderLayout.NORTH);
+		animationPanel = new JPanel(con.getFlowLayout(1,0,0));
+		animationPanel.setBackground(new Color(153,153,153));
+		flowchart = new MyLabel(con,"flowchart.png",rightwidth,height-initalMenuPanel.getHeight());
+		animationPanel.add(flowchart);
+		p_right.add(animationPanel,BorderLayout.CENTER);
+		this.add(p_right);
+		
+		
+		
+	}
+
+
+}
---
+++ trunk/InitalMenuPanel.java
@@ -0,0 +1,54 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+
+public class InitalMenuPanel extends JPanel{
+	MyLabel benchmarkFunctionInfo,heuristicSelectionInfo, lowLevelHeuristicsInfo, acceptanceMethodInfo, launchDemonstrationInfo;
+	MyButton benchmarkFunction, heuristicSelection, lowLevelHeuristics, acceptanceMethod, launchDemonstration;
+	JPanel left,center,right;
+	boolean benchmarkFunctionMenu = true,heuristicSelectionMenu = false,lowLevelHeuristicsMenu = false, acceptanceMethodMenu = false;
+	public InitalMenuPanel(int width,Config con,VSHMainFrame frame){
+		this.setOpaque(false);
+		//this.setPreferredSize(new Dimension(width,28));
+		left = new JPanel();
+		left.setBackground(con.getLeftBgColor());
+		this.setLayout(new BorderLayout());
+		this.add(left,BorderLayout.WEST);
+		center = new JPanel();
+		center.setLayout(con.getFlowLayout(1,0,0));
+		center.setBackground(con.getLeftBgColor());
+		benchmarkFunction = new MyButton(con,"benchmark_function_on.png","benchmark_function_pressed.png","Benchmark Function");
+		benchmarkFunction.setActionCommand("Benchmark Function");
+		benchmarkFunction.addActionListener(new Action(frame,benchmarkFunction));
+		heuristicSelection = new MyButton(con,"heuristic_selection_on.png","heuristic_selection_pressed.png","Heuristic Selection");
+		heuristicSelection.setActionCommand("Heuristic Selection");
+		heuristicSelection.addActionListener(new Action(frame,heuristicSelection));
+		lowLevelHeuristics = new MyButton(con,"low_level_on.png","low_level_pressed.png","Low Level Heuristics");
+		lowLevelHeuristics.setActionCommand("Low Level Heuristics");
+		lowLevelHeuristics.addActionListener(new Action(frame,lowLevelHeuristics));
+		acceptanceMethod = new MyButton(con,"acceptance_method_on.png","acceptance_method_pressed.png","Acceptance Method");
+		acceptanceMethod.setActionCommand("Acceptance Method");
+		acceptanceMethod.addActionListener(new Action(frame,acceptanceMethod));
+		benchmarkFunctionInfo  = new MyLabel("Benchmark Function",con);
+		heuristicSelectionInfo = new MyLabel("Heuristic Selection",con);
+		lowLevelHeuristicsInfo = new MyLabel("Low Level Heuristics",con);
+		acceptanceMethodInfo = new MyLabel("Acceptance Method",con);
+		center.add(benchmarkFunction);
+		center.add(benchmarkFunctionInfo);
+		center.add(heuristicSelection);
+		center.add(heuristicSelectionInfo);
+		center.add(lowLevelHeuristics);
+		center.add(lowLevelHeuristicsInfo);
+		center.add(acceptanceMethod);
+		center.add(acceptanceMethodInfo);
+		this.add(center,BorderLayout.CENTER);
+		
+		right = new JPanel();
+		right.setBackground(con.getLeftBgColor());
+		this.add(right,BorderLayout.EAST);
+	}
+	
+}
