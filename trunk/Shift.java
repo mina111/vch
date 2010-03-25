28ba852e54c4960144004b81c582b4fef30fee27
laojingqi <laojingqi@a8ac134c-b99d-11de-b431-8336cfaced44>
2010-03-25 12:30:30 +0000

    Low Level Huristics are done.

--- trunk/Shift.java
+++ trunk/Shift.java
@@ -4,7 +4,35 @@ public class Shift extends LowLevelHeuristic{
 	@Override
 	int[] generateNewSoluation(int[] candidateSoluation) {
 		// TODO Auto-generated method stub
-		return null;
+		int newSoluatuon[] = new int[candidateSoluation.length];
+		for(int i =0;i<candidateSoluation.length;i++)
+			newSoluatuon[i]=candidateSoluation[i];
+		int modifiedDigitNum = getModifiedDigitNum();
+		int startPostion = getStartPostion();
+		int endPostion = getEndPostion(startPostion,modifiedDigitNum);
+		if(startPostion<endPostion){
+			int temp = newSoluatuon[startPostion];
+			for(int i = startPostion;i<endPostion;i++){
+				newSoluatuon[i]= newSoluatuon[i+1];
+			}
+			newSoluatuon[endPostion] = temp;
+			return newSoluatuon;
+		}else{
+			int temp = newSoluatuon[startPostion];
+			for(int i = startPostion;i<HyperHeuristic.DIGIT_NUM ;i++){
+				if(i==HyperHeuristic.DIGIT_NUM -1){
+					newSoluatuon[i]=newSoluatuon[0];
+				}else{
+					newSoluatuon[i]= newSoluatuon[i+1];
+				}
+			}
+			for(int i = 0;i<endPostion;i++){				
+				newSoluatuon[i]= newSoluatuon[i+1];
+			}
+			newSoluatuon[endPostion] = temp;
+			return newSoluatuon;
+		}
+		
 	}
 
 	@Override
