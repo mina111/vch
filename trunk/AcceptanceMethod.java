e3856a89d69fa33b709048aca0deae4d842cacb8
laojingqi <laojingqi@a8ac134c-b99d-11de-b431-8336cfaced44>
2010-04-01 14:06:50 +0000

    Add some comments

--- trunk/AcceptanceMethod.java
+++ trunk/AcceptanceMethod.java
@@ -1,7 +1,39 @@
+
+/**
+ *  A Visualisation Tool for 
+ *  Selection Hyper-Heuristics                                    <br>   
+ *  															  <br>
+ *  http://code.google.com/p/vch/   							  <br>
+ *  															  <br>
+ *  Module:                  G52GRP, University of Nottingham     <br>
+ *  															  <br>
+ *  Group:        gp09-exo    						  			  <br>
+ * @author 	   	Lao Jingqi (jxl29u)
+ * @author	   	Zhang Chao (cxz09u)
+ * @author		Thomas Barton (txb18u)
+ * @author		Ben Jenkinson (bxj08u)
+ * @author	   	Alexander Jermstad (asj08u) 
+ * 			
+ */
+
+/**
+ *  This class abstract the method for all acceptance methods
+ */
 public abstract class AcceptanceMethod {
 
+	/**
+	 *  get the name of accept method.
+	 * @return
+	 */
 	abstract String getName();
-
+	
+	/**
+	 *  check if accept the new solution or not
+	 * @param candidate_Solution
+	 * @param new_Solution
+	 * @return a boolean value to decide whether accept the new solution or not
+	 */
 	abstract boolean checkIfAcceptance(int[] candidate_Solution, int[] new_Solution);
+
 
 }
--- trunk/GreedyRandom.java
+++ trunk/GreedyRandom.java
@@ -1,21 +1,65 @@
 
+/**
+ *  A Visualisation Tool for 
+ *  Selection Hyper-Heuristics                                    <br>   
+ *  															  <br>
+ *  http://code.google.com/p/vch/   							  <br>
+ *  															  <br>
+ *  Module:                  G52GRP, University of Nottingham     <br>
+ *  															  <br>
+ *  Group:        gp09-exo    						  			  <br>
+ * @author 	   	Lao Jingqi (jxl29u)
+ * @author	   	Zhang Chao (cxz09u)
+ * @author		Thomas Barton (txb18u)
+ * @author		Ben Jenkinson (bxj08u)
+ * @author	   	Alexander Jermstad (asj08u) 
+ * 			
+ */
+
+/**
+ *  the class is  the heuristics selection greedy random which extends from HeuristicsSelection
+ */
 public class GreedyRandom extends HeuristicsSelection {
+	/**
+	 *  the instance of HyperHeuristic
+	 */
 	HyperHeuristic hyperHeuristic;
+	/**
+	 *  to store the optimum solution
+	 */
 	int[] optimumSolution;
-	int[][] histories;
+	/**
+	 *  to store the histories of new solution
+	 */
+	int[][] histories ;
+	/**
+	 *  the index of optimum solution
+	 */
 	int optimumSolutionIndex;
+	
+	/**
+	 * Defines an GreedyRandom object with a instance of HyperHeuristic 
+	 * @param hyperHeuristic
+	 */
 	public GreedyRandom(HyperHeuristic hyperHeuristic) {
 		// TODO Auto-generated constructor stub
 		this.hyperHeuristic = hyperHeuristic;
 		histories = new int[hyperHeuristic.lowLevelHeuristics.size()][HyperHeuristic.DIGIT_NUM];
 	}
 
+	
+	/**
+	 *  return the name as "Greedy Random"
+	 */
 	@Override
 	String getName() {
 		// TODO Auto-generated method stub
 		return "Greedy Random";
 	}
 
+	/**
+	 * Override the method to get a low-level heuristic by greedy random
+	 */
 	@Override
 	LowLevelHeuristic selectLowLevelHeuristic(int[] candiateSolution) {
 		// TODO Auto-generated method stub
@@ -24,15 +68,15 @@ public class GreedyRandom extends HeuristicsSelection {
 		optimumSolution = lowLevelHeuristic.generateNewSolution(candiateSolution);
 		histories[0] = optimumSolution;
 		for(int i=1; i<hyperHeuristic.lowLevelHeuristics.size();i++){
-			int[] newSolution = hyperHeuristic.lowLevelHeuristics.get(i).generateNewSolution(candiateSolution);
-			histories[i] = newSolution;
-			if( hyperHeuristic.function.evaluate(newSolution) <= hyperHeuristic.function.evaluate(optimumSolution)    ){
+			int[] newSoluation = hyperHeuristic.lowLevelHeuristics.get(i).generateNewSolution(candiateSolution);
+			histories[i] = newSoluation;
+			if( hyperHeuristic.function.evaluate(newSoluation) <= hyperHeuristic.function.evaluate(optimumSolution)    ){
 				lowLevelHeuristic = hyperHeuristic.lowLevelHeuristics.get(i);
-				optimumSolution = newSolution;
+				optimumSolution = newSoluation;
 				optimumSolutionIndex = i;
 			}
-
-
+				
+				
 		}
 		return lowLevelHeuristic;
 	}
