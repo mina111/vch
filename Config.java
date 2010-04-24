8155067a88770e04950244df458ae0fc42b959da
laojingqi <laojingqi@a8ac134c-b99d-11de-b431-8336cfaced44>
2010-04-24 18:00:23 +0000

    Add info window.

--- Config.java
+++ Config.java
@@ -14,88 +14,68 @@ import javax.swing.UIManager;
 
 public class Config
 {
-	//===========================
-	//一些初始化参数
-	//===========================
-	String skin_dir , softtitle ;//软件皮肤 | 上面的标题文字
-	int width,height;//初始窗体宽高
-	int leftmenuwidth , splitbarwidth ; //主要区域,左二级菜单宽度|左右分格面板的宽度
-	boolean iffulltop , ifnormaltop , openfull , lmenucanclose; //窗体最大时总前端显示|正常模式总前端显示|启动时左边菜单是否显示|左边是否可以关闭
+
+	String skin_dir , softtitle ;
+	int width,height;
+	int leftmenuwidth , splitbarwidth ; 
+	boolean iffulltop , ifnormaltop , openfull , lmenucanclose; 
 	private int splitbarHeight;
 
-	//===========================
-	//初始化
+
 	Config()
 	{
-		skin_dir = "icon";//软件皮肤文件夹
-		width = 1024 ; //窗体宽
-		height = 728 ; //窗体高
-		iffulltop = true ; //全屏幕是否在最前端显示
-		ifnormaltop = false ; //非全屏是否在最前端显示
-		leftmenuwidth = 150 ; //左边二级菜单宽度
-		splitbarwidth = 6 ; //分格面板宽度
+		skin_dir = "icon";
+		width = 1024 ; 
+		height = 728 ; 
+		iffulltop = true ; 
+		ifnormaltop = false ; 
+		leftmenuwidth = 150 ; 
+		splitbarwidth = 6 ; 
 		splitbarHeight = 6;
-		openfull = false ; //软件启动时就全屏
+		openfull = false ; 
 		softtitle = "Visualisation Tool for a Selection Hyper-Heuristic (code.google.com/p/vch)";
-		lmenucanclose = true; //是否可以关闭左边菜单栏
+		lmenucanclose = true; 
 	}
-	//===========================
-	//返回皮肤文件夹
+
 	public String getSkinDir()
 	{
-		return skin_dir;//返回皮肤文件夹
+		return skin_dir;
 	}
-	//=================================
-	//重置窗体大小
-	//=================================
 
-	// ==================================
-	// 返回包中指定图片函数
-	// ==================================
 	public ImageIcon getImgUrl(String img)
 	{
 		return new ImageIcon(VSHMainFrame.class.getResource(getSkinDir() + "/" + img));
 	}
-	// ==================================
-	// 返回窗体最小尺寸
-	// ==================================
+
 	public Dimension getFrameSmallSize(boolean full)
 	{
-		if(!full)//不是最大化初始
+		if(!full)
 		{
 			return new Dimension(width,height);
 		}
-		else//全屏
+		else
 		{
 			Toolkit kit = Toolkit.getDefaultToolkit();
 			return new Dimension(kit.getScreenSize());
 		}
 	}
-	// ==================================
-	// 返回分辨率
-	// ==================================
+
 	public Dimension getScreenSize()
 	{
 		Toolkit kit = Toolkit.getDefaultToolkit();
 		return new Dimension(kit.getScreenSize());
 	}
-	// ==================================
-	//  最大化时是否显示在前端,如考试系统不许打开其它东西
-	// ==================================
+
 	public void fullAllWaysTop(VSHMainFrame frame)
 	{
 		frame.setAlwaysOnTop(iffulltop);
 	}
-	// ==================================
-	//  正常时是否显示在前端,如考试系统不许打开其它东西
-	// ==================================
+
 	public void normalAllWaysTop(VSHMainFrame frame)
 	{
 		frame.setAlwaysOnTop(ifnormaltop);
 	}
-	// ==================================
-	//  返回布局对象,参数:0左,1中,2右
-	// ==================================
+
 	public FlowLayout getFlowLayout(int flag,int hgap,int vgap)
 	{
 		FlowLayout fl = new FlowLayout();
@@ -104,44 +84,32 @@ public class Config
 		fl.setAlignment(flag);
 		return fl;
 	}
-	//===================================
-	//返回左边二级菜单面板的宽度
-	//===================================
+
 	public int getLeftMenuWidth()
 	{
 		return this.leftmenuwidth;
 	}
-	//===================================
-	//返回分格面板宽度
-	//===================================
+
 	public int getSplitBarWidth()
 	{
 		return this.splitbarwidth;
 	}
-	//===================================
-	//返回左边菜单背景
-	//===================================
+
 	public Color getLeftBgColor()
 	{
 		return new Color(50,52,55);
 	}
-	//===================================
-	//返回分格页面背景色
-	//===================================
+
 	public Color getSplitBgColor()
 	{
 		return new Color(30,30,30);
 	}
-	//===================================
-	//返回右边区域色彩
-	//===================================
+
 	public Color getMainColor()
 	{
 		return new Color(55,55,55);
 	}
-	//===================================
-	//初始化UI
-	//===================================
+
 	public void setFrameUI()
 	{
 		Font font = new Font("宋体", Font.PLAIN, 12);
@@ -151,40 +119,7 @@ public class Config
 		Color gray = new Color(240,240,240);
 		Color black = new Color(0,0,0);
 
-       /* UIManager.put("ScrollBar.track", black);
-        UIManager.put("ScrollBar.trackHighlight",black);
 
-        UIManager.put("ScrollBar.background", gray);
-        UIManager.put("ScrollBar.shadow", gray);
-        UIManager.put("ScrollBar.darkShadow", gray);
-
-        UIManager.put("ScrollBar.thumb", gray);
-        UIManager.put("ScrollBar.thumbShadow",gray);
-        UIManager.put("ScrollBar.thumbDarkShadow",gray);
-        UIManager.put("ScrollBar.thumbHighlight", new Color(250,250,250));
-        //UIManager.put("ScrollBar.thumbLightShadow", new Color(250,60,250));
-        UIManager.put("ScrollBar.width", 17);
-
-        */
-
-        /*
-        ScrollBar.background
-		ScrollBar.darkShadow
-		ScrollBar.focusInputMap
-		ScrollBar.foreground
-		ScrollBar.highlight
-		ScrollBar.maximumThumbSize
-		ScrollBar.minimumThumbSize
-		ScrollBar.shadow
-		ScrollBar.thumb
-		ScrollBar.thumbDarkShadow
-		ScrollBar.thumbHighlight
-		ScrollBar.thumbLightShadow
-		ScrollBar.thumbShadow
-		ScrollBar.track
-		ScrollBar.trackHighlight
-		ScrollBar.width
-         */
 
 
 	}
---
+++ InfoFrame.java
@@ -0,0 +1,42 @@
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JFrame;
+
+
+public class InfoFrame extends JFrame{
+	
+	public InfoFrame(Config con, VSHMainFrame mainFrame){
+		setTitle("About");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(VSHMainFrame.class.getResource(con.getSkinDir() + "/" + "icon1.png")));
+		Point mainFramePoint = mainFrame.getLocation();
+		setSize(300, 250);
+		setLocation((mainFrame.getSize().width-300)/2+mainFramePoint.x, (mainFrame.getSize().height-250)/2+mainFramePoint.y);
+		InfoPanel infoPanel = new InfoPanel(con,mainFrame);
+		add(infoPanel);
+		final Point point = new Point();
+		infoPanel.toppanel.addMouseListener (new MouseAdapter() 
+		{
+			public void mousePressed(MouseEvent e) 
+			{
+				point.x = e.getX();
+				point.y = e.getY();
+			}
+		});
+		infoPanel.toppanel.addMouseMotionListener(new MouseMotionAdapter()
+		{
+			public void mouseDragged(MouseEvent e)
+			{
+
+					Point tpoint = getLocation();
+					setLocation(tpoint.x + e.getX() - point.x, tpoint.y + e.getY() - point.y );
+			}
+		});
+		setUndecorated(true);
+		setVisible(true);
+	}
+}
